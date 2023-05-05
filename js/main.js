fc433ac21750770720ddd2364047d031716d6c4e
HUAWEI <mabar@tut.by>
2023-05-05 20:37:37 +0300

    sideBar added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
 const searchInput = document.getElementById("header-search_input");
+const classSelect = document.getElementById("class-select");
+const menu = document.getElementById("menu");
+const ellipse = document.querySelector(".ellipse");
 
+// main input
 searchInput.addEventListener("input", () => {
-  console.log(searchInput.value);
+  console.log(searchInput.value); //value for backend
+});
+
+classSelect.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  classSelect.classList.toggle("active");
+
+  function addClass() {
+    ellipse.classList.toggle("active");
+  }
+  ellipse.classList.contains("active") ? setTimeout(addClass, 800) : ellipse.classList.add("active");
+
+  document.addEventListener("click", (e) => {
+    const isClickInsideMenu = menu.contains(e.target);
+    const isClickInsideClassSelect = classSelect.contains(e.target);
+    if (!isClickInsideMenu && !isClickInsideClassSelect) {
+      function removeClass() {
+        ellipse.classList.remove("active");
+      }
+      setTimeout(removeClass, 800);
+      menu.classList.remove("active");
+      classSelect.classList.remove("active");
+    }
+  });
 });
