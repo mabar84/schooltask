9fe0ebc482e0ffc8b89d8bf63f6d63664e17e623
HUAWEI <mabar@tut.by>
2023-05-06 03:51:42 +0300

    i'm doing mobile menu

--- js/main.js
+++ js/main.js
@@ -4,11 +4,24 @@ const menu = document.getElementById("menu");
 const ellipse = document.querySelector(".ellipse");
 const classSselectItems = menu.querySelectorAll(".class-select_item");
 const allMenuAdditional = document.querySelectorAll(".menu-additional");
+const navButtons = document.querySelectorAll(".nav-button");
+const navButtonsMobile = document.querySelector(".nav-buttons_mobile");
+const navMenuMobile = document.querySelector(".nav-menu_mobile");
 
 // main input
 searchInput.addEventListener("input", () => {
   console.log(searchInput.value); //value for backend
 });
+// site-navigate
+navButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    navButtons.forEach((el) => {
+      el.classList.remove("active");
+    });
+    btn.classList.add("active");
+  });
+});
+
 // show/close menu
 classSelect.addEventListener("click", () => {
   menu.classList.toggle("active");
@@ -53,5 +66,22 @@ classSselectItems.forEach((el) => {
       el.classList.remove("active");
     });
     menuAdditional.classList.add("active");
+  });
+});
+
+// show/close mobile menu
+navButtonsMobile.addEventListener("click", () => {
+  navMenuMobile.classList.toggle("active");
+
+  document.addEventListener("click", (e) => {
+    const isClickInsideMobileMenu = navMenuMobile.contains(e.target);
+    console.log(isClickInsideMobileMenu);
+
+    const isClickInsideMenu = navMenuMobile.contains(e.target);
+    const isClickInsideNavButtons = navButtonsMobile.contains(e.target);
+
+    if (!isClickInsideMenu && !isClickInsideNavButtons) {
+      navMenuMobile.classList.remove("active");
+    }
   });
 });
